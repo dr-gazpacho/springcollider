cb9799050ef0c8485aa1b5e5f3e673b510f40208
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-01-13 20:08:29 -0600

    initial commit

---
+++ src/main/kotlin/com/gazpacho/schtmx/SchtmxApplication.kt
@@ -0,0 +1,11 @@
+package com.gazpacho.schtmx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchtmxApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchtmxApplication>(*args)
+}
---
+++ src/test/kotlin/com/gazpacho/schtmx/SchtmxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gazpacho.schtmx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchtmxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

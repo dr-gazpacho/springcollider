93bf272eca8a62d89884dacb27593f84ba9cf28f
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-01-17 13:36:54 -0600

    update controller

--- src/main/kotlin/com/gazpacho/springcollider/controller/api/ApiController.kt
+++ src/main/kotlin/com/gazpacho/springcollider/controller/api/ApiController.kt
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-// Separate controller for API endpoints
 @RestController
-@RequestMapping("/api")  // All API routes go under /api/*
+@RequestMapping("/api")
 class ApiController {
     @GetMapping("/cakes")
     fun getCakes(): String {
@@ -20,5 +19,11 @@ class ApiController {
     fun createCake(@RequestBody cake: String): String {
         print("cake post")
         return ""
+    }
+
+    @GetMapping("/test")
+    fun handleTest(): Map<String, String> {
+        print("incoming get was tight dude")
+        return mapOf("message" to "it was tight indeed")
     }
 }

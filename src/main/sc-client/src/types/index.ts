0e97606053c5d9ab041e1331b54ba2b5fe22958c
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-01-22 15:34:20 -0600

    move types

---
+++ src/main/sc-client/src/types/index.ts
@@ -0,0 +1,35 @@
+export type SliderConfig = {
+    symbol: string,
+    value: number,
+    endpoint: ENDPOINT,
+    httpMethod: HTTPMETHOD,
+    min: number,
+    max: number,
+}
+
+export enum SliderVariant {
+    FREQUENCY = "FREQUENCY",
+    VOLUME = "VOLUME"
+}
+
+export enum HTTPMETHOD {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE"
+};
+
+export enum ENDPOINT {
+    VOLUME = "volume",
+    FREQ = "frequency",
+    CREATETEST = "createTest",
+    FREE = "free"
+}
+
+export type Configuration = {
+    method: HTTPMETHOD;
+    headers: {
+        [key: string]: string
+    }
+    body?: any;
+}
